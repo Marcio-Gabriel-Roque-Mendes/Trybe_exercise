2dd94218f9b25aea06cd18f892d59b7e16b0e161
￼Marcio-Gabriel-Roque-Mendes <marcio_2411@hotmail.com>
2023-01-19 16:27:32 -0300

    Adição do conteudo de entrada e saida

---
+++ Exercicios/4 - Ciencia da Computacao/bloco-31-introducao-a-python/dia-2-entrada-e-saida-de-dados/conteudo-1-entrada-e-saida/saida_1.py
@@ -0,0 +1,20 @@
+# SAIDA
+
+# PRINT 
+# a função print — que já vem embutida na linguagem — é a principal função para imprimirmos um valor em “tela”.
+
+# O separador padrão dos argumentos é um espaço em branco, que pode ser alterado.
+
+print("Maria", "João", "Miguel", "Ana")  # saída: Maria João Miguel Ana
+print("Maria", "João", "Miguel", "Ana", sep=", ")  # saída: Maria, João, Miguel, Ana
+
+# Além do separador, podemos também alterar o caractere de fim de linha que, por regra, é uma quebra de linha:
+
+print("Em duas ")
+print("linhas.")    
+
+# Alterando o padrão:
+
+print("Na mesma", end=" ")
+print("linha.")
+
