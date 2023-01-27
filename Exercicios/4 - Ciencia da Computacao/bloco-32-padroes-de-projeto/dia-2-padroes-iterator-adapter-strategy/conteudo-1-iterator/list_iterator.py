9dbba13c4f6a2a898e7d47369364ad1f6147b774
￼Marcio-Gabriel-Roque-Mendes <marcio_2411@hotmail.com>
2023-01-27 09:38:08 -0300

    Adição do conteudo do padrão de projeto Iterator

---
+++ Exercicios/4 - Ciencia da Computacao/bloco-32-padroes-de-projeto/dia-2-padroes-iterator-adapter-strategy/conteudo-1-iterator/list_iterator.py
@@ -0,0 +1,11 @@
+times_paulistas = ['São paulo', 'Corinthians', 'Palmeiras', 'Santos']
+
+iterando = iter(times_paulistas)
+print(next(iterando))
+# saída: São paulo
+print(next(iterando))
+# saída: Corinthians
+print(next(iterando))
+# saída: Palmeiras
+print(next(iterando))
+# saída: Santos
