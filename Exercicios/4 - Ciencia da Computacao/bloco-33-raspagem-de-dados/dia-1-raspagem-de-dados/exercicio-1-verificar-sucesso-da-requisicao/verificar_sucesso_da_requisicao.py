9d8f7d25bf8a04e15875e72e0be90e8af9879f00
￼Marcio-Gabriel-Roque-Mendes <marcio_2411@hotmail.com>
2023-02-15 17:51:45 -0300

    Adição de arquivos não commitados

--- Exercicios/4 - Ciencia da Computacao/bloco-33-raspagem-de-dados/dia-1-raspagem-de-dados/exercicio-1-verificar-sucesso-da-requisicao/verificar_sucesso_da_requisicao.py
+++ Exercicios/4 - Ciencia da Computacao/bloco-33-raspagem-de-dados/dia-1-raspagem-de-dados/exercicio-1-verificar-sucesso-da-requisicao/verificar_sucesso_da_requisicao.py
@@ -1,7 +1,8 @@
 import requests
 
 response = requests.get(
-    "https://scrapethissite.com/pages/advanced/?gotcha=headers"
+    "https://scrapethissite.com/pages/advanced/?gotcha=headers",
+    headers={"User-agent": "Mozilla", "Accept": "text/html"},
 )
 
 print(response.text)
