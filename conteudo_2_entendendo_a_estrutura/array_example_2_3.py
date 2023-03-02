a885ae6217f2dd462d817bb8a5c6e865d852422f
￼Marcio-Gabriel-Roque-Mendes <marcio_2411@hotmail.com>
2023-03-02 20:37:25 -0300

    Conteudo 2 - Entendendo a Estrutura

---
+++ conteudo_2_entendendo_a_estrutura/array_example_2_3.py
@@ -0,0 +1,41 @@
+# import sys
+
+
+class ListaArray:
+    def __init__(self):
+        self.data = []
+
+    def __len__(self):
+        # quando pedido o tamanho do array
+        # retorne o tamanho de data
+        return len(self.data)
+
+    def __str__(self):
+        # converte para string e exibe os valores de data
+        return str(self.data)
+
+    def get(self, index):
+        # recupera o elemento no índice informado
+        return self.data[index]
+
+    def set(self, index, value):
+        # insere um elemento no índice informado
+        self.data.insert(index, value)
+
+    def remove(self, index):
+        # removeremos o item, retornando-o
+        return self.data.pop(index)
+
+# RESOLUCAO DO EXERCICIO DE FIXACAO
+    def update(self, index, value):
+        update = self.data[index] = value
+        return update
+
+
+array = ListaArray()
+array.set(0, "Marcos")
+array.set(1, "Patrícia")
+print(array)  # saída: ['Marcos', 'Patrícia']
+
+array.remove(0)  # retorna a string "Marcos"
+print(array)  # saída: ['Patrícia']
